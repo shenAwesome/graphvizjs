0c33f1fc007026ee97a55c41ca2cb786e7945eb6
Shen Z <shenAwesome@gmail.com>
2022-07-30 18:50:34 +1000

    gg

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,8 @@ class Viz {
         return this.graphviz.layout(dot, format, engine) as string
     }
 
-    createLayoutManager(rankdir: Rankdir = "TB") {
-        return new LayoutManager(this, rankdir)
+    createLayoutManager() {
+        return new LayoutManager(this)
     }
 }
 
@@ -26,6 +26,8 @@ class Node {
     constructor(public readonly id: string, public width: number, public height: number) {
 
     }
+    callback: (x: number, y: number) => void
+
     toString() {
         const { id, width, height } = this
         return `${id} [width="${width}",height="${height}"]; `
@@ -61,65 +63,79 @@ class LayoutManager {
 
     private idMap = {} as { [string: string]: string }
 
-    constructor(private viz: Viz, private rankdir: Rankdir) {
+    constructor(private viz: Viz) {
     }
 
-    addNode(id: string, width: number, height: number) {
+    /**
+     * Add an node and return id. id is for referring this node when using addEge or addGroup.
+     * @param width 
+     * @param height 
+     * @param id if an id is not given in option, one will be created 
+     * @param callback called on LayoutManager.calculate()
+     * @returns 
+     */
+    addNode(width: number, height: number, id: string, callback?: (x: number, y: number) => void) {
         const autoId = (this.nodes.length + 1).toString()
+        id = id || autoId
         this.idMap[id] = autoId
-        this.nodes.push(new Node(autoId, this.toInch(width), this.toInch(height)))
-        return this
+        const node = new Node(autoId, this.toInch(width), this.toInch(height))
+        node.callback = callback
+        this.nodes.push(node)
+        return node.id
     }
+
     addEdge(from: string, to: string) {
         from = this.idMap[from]
         to = this.idMap[to]
         this.edges.push(new Edge(from, to))
-        return this
-    }
-    addGroup(ids: string[]) {
-        this.groups.push(new Group(this.groups.length + '', ids.map(id => this.idMap[id])))
-        return this
     }
 
-    calculate() {
+    addGroup(ids: string[]) {
+        this.groups.push(new Group(this.groups.length + '', ids.map(id => this.idMap[id])))
+    }
+
+    calculate(rankdir: Rankdir = "TB") {
+        const engine = this.edges.length > 0 ? 'dot' : 'neato'
         const dot = `
         digraph D { 
             node [shape=record]
-            rankdir = "${this.rankdir}"
+            rankdir = "${rankdir}"
             ${this.nodes.map(n => n.toString()).join(' ')}
             ${this.edges.map(n => n.toString()).join(' ')}
             ${this.groups.map(n => n.toString()).join(' ')}
         } `
-        const out = this.viz.layout(dot, 'plain')
-        const lines = out.split('\n')
-        const { toPx } = this
-        const paperInfo = lines[0].split(' ')
-        const height = toPx(paperInfo.pop()),
+        const out = this.viz.layout(dot, 'plain', engine),
+            svg = this.viz.layout(dot, 'svg', engine),
+            lines = out.split('\n'),
+            { toPx } = this,
+            paperInfo = lines[0].split(' '),
+            height = toPx(paperInfo.pop()),
             width = toPx(paperInfo.pop()),
-            nodes = [] as any[]
-
-        const idmap2 = {} as { [key: string]: string }
+            nodes = [] as any[],
+            idmap2 = {} as { [key: string]: string }
         Object.keys(this.idMap).forEach(k => idmap2[this.idMap[k]] = k)
 
         for (const line of lines.slice(1, -2)) {
             const [type] = line.split(' ')
             if (type == 'node') {
-                const [type, id, x, y, w, h] = line.split(' ')
+                const [_, idx, x, y, w, h] = line.split(' ')
                 const node = {
-                    id: idmap2[id],
-                    index: parseInt(id),
+                    id: idmap2[idx],
+                    index: parseInt(idx),
                     x: toPx(x),
                     y: height - toPx(y),
                     width: toPx(w),
                     height: toPx(h),
                 }
-                node.x = Math.round(node.x - node.width / 2)
-                node.y = Math.round(node.y - node.height / 2)
+                node.x = Math.max(0, Math.round(node.x - node.width / 2))
+                node.y = Math.max(0, Math.round(node.y - node.height / 2))
                 nodes.push(node)
+                const _node = this.nodes.find(n => n.id == idx)
+                if (_node && _node.callback) _node.callback(node.x, node.y)
             }
         }
         nodes.sort((a, b) => a.index - b.index)
-        return { width, height, nodes }
+        return { width, height, nodes, svg }
     }
 
     toInch(px: number) {
@@ -128,18 +144,6 @@ class LayoutManager {
 
     toPx(inch: number | string) {
         return Math.round(parseFloat(inch + '') * 96)
-    }
-
-    toSVG() {
-        const dot = `
-        digraph D { 
-            node [shape=record]
-            rankdir = "${this.rankdir}"
-            ${this.nodes.map(n => n.toString()).join(' ')}
-            ${this.edges.map(n => n.toString()).join(' ')}
-            ${this.groups.map(n => n.toString()).join(' ')}
-        } `
-        return this.viz.layout(dot, 'svg')
     }
 }
 
